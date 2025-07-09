e51c75c8a1d4ae3e8e04485419c2b4803bfa9b95
Hamza <hamzaiftikhar@iaanswers.com>
2025-07-09 18:35:03 +0500

    time zone fix

--- util/getCountryInfoFromPhone.js
+++ util/getCountryInfoFromPhone.js
@@ -5,7 +5,7 @@ import countryTimezone from 'country-timezone';
 /**
  * Get country and timezone info from a phone number
  * @param {string} phoneNumberRaw - e.g. "+923001234567"
- * @returns {Object|null} - { countryCode, countryName, timezone } or null if invalid
+ * @returns {Object|null} - { countryCode, timezone } or null if invalid
  */
 export function getCountryInfoFromPhone(phoneNumberRaw) {
   try {
@@ -13,12 +13,10 @@ export function getCountryInfoFromPhone(phoneNumberRaw) {
     if (!phoneNumber || !phoneNumber.country) return null;
 
     const countryCode = phoneNumber.country;
-    const countryName = countryTimezone.getCountryName(countryCode);
-    const timezones = countryTimezone.getTimezones(countryCode);
+    const timezones = countryTimezone.getTimezonesWithCountryCode(countryCode);
 
     return {
       countryCode,
-      countryName,
       timezone: timezones?.[0] || null,
     };
   } catch (err) {
