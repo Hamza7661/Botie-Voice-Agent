9ceec9aa85888662910cb1b7debeaf9cc80d5d7b
Hamza <hamzaiftikhar@iaanswers.com>
2025-07-09 18:40:01 +0500

    contact info log

--- server.js
+++ server.js
@@ -124,8 +124,8 @@ async function summarizeConversation(convo, callerPhoneNumber, tradie) {
   - summary: Brief job description  
   - description: Description of the task/issue (not the full conversation)
   - reminder: Reminder text if the user is setting a reminder if not set it to null
-  - reminderLocation: Location (lat, long) of the reminder if the user is setting a reminder and mentioned a location if not set it to null. Set the mentioned location's lat long according to country ${contactInfo?.countryCode || 'unknown'}
-  - reminderTime: Time of the reminder if the user is setting a reminder and mentioned date or a time if not set it to null. Its type is dateTime. Date should be current date if not mentioned else the mentioned date. It cannot be in the past. Today is ${new Date().toString()}. Also convert the mentioned time into utc using the timezone ${contactInfo?.timezone || 'UTC'}
+  - reminderLocation: Location (lat, long) of the reminder if the user is setting a reminder and mentioned a location if not set it to null. Set the mentioned location's lat long according to country code ${contactInfo?.countryCode || ''}
+  - reminderTime: Time of the reminder if the user is setting a reminder and mentioned date or a time if not set it to null. Its type is dateTime. Date should be current date if not mentioned else the mentioned date. It cannot be in the past. Today is ${new Date().toString()}. Also convert the mentioned time into utc. The user said the time in timezone ${contactInfo?.timezone || ''}
   - conversation: The complete conversation as a string
   - customer: { name, address, phoneNumber: "${callerPhoneNumber || ''}" }
   - isResolved: false
